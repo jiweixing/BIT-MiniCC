61dbf477bf6fe4d5b5a4012ceb05da673b61e3b6
marche147 <i@shiki7.me>
2019-04-13 19:53:04 +0800

    Fix test case & obfuscation

--- input/test.c
+++ input/test.c
@@ -3,15 +3,15 @@
 /* this is a demo program */
 
 int main(){
-	int a;
-	int b;
-	
-	a = MARS_SCANF_I();
-	b = MARS_SCANF_I();
-	
-	a = a + b;
-	
-	MARS_PRINTF_I(a);
-	
-	return 0;
+  int a;
+  int b;
+  
+  a = MARS_GETI();
+  b = MARS_GETI();
+  
+  a = a + b;
+  
+  MARS_PUTI(a);
+  
+  return 0;
 }
